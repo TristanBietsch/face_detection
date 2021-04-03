f30df2907e9845915bff62b4a4ed0e45221fe0ae
Tristan_Bietsch <tristanbietsch@gmail.com>
2021-04-03 17:49:55 -0500

    finished product

--- face_detector.py
+++ face_detector.py
@@ -1,26 +1,48 @@
 import cv2
+from random import randrange
 
 #Load pre-trained data on face frontals
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Choose image to detect faces in
-#img = cv2.imread('RDJ.jpg')
-img = cv2.imread('T.jpeg')
+# example: img = cv2.imread('T.jpeg')
 
-#Convert img to grayscale
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Choose video via webcam to detect faces in
+webcam = cv2.VideoCapture(0)
+key = cv2.waitKey(1)
 
-#Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+#iterate forever on frames
+while True:
+    
+    ###Read current frame
+    sucessful_frame_read, frame = webcam.read()
 
-#Draw rectangle around faces
-(x, y, w, h) = face_coordinates[0]
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 225, 0), 10)
+    #Convert img to grayscale
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    
+    #Draw rectangle around faces
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 256), 10)
+        
+    #Frame output name
+    cv2.imshow("Tristan's Face Detector", frame)
+    cv2.waitKey(1)
+    
+   # ### Stop program if X key is pressed
+    #if key==88 or key==120:
+    #    break
+    
+# Release VideoCapture Obj
+webcam.release()
 
-print(face_coordinates)
 
-#
-cv2.imshow("Tristan's Face Detector Program", img)
-cv2.waitKey()
+
+
+
+
+
 
 print("Code sussesfully compiled!")
