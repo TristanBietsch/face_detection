5c2a44699cf6f26b15ec113dc99dc68c9c2bccfa
Tristan_Bietsch <tristanbietsch@gmail.com>
2021-04-03 17:10:34 -0500

    second commit

--- face_detector.py
+++ face_detector.py
@@ -1,12 +1,26 @@
 import cv2
 
+#Load pre-trained data on face frontals
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+#Choose image to detect faces in
+#img = cv2.imread('RDJ.jpg')
+img = cv2.imread('T.jpeg')
 
+#Convert img to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#Detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
+#Draw rectangle around faces
+(x, y, w, h) = face_coordinates[0]
+cv2.rectangle(img, (x, y), (x+w, y+h), (0, 225, 0), 10)
 
+print(face_coordinates)
 
+#
+cv2.imshow("Tristan's Face Detector Program", img)
+cv2.waitKey()
 
-
-#Message to show compiled code has no errors
-print("Code sussesfully compliled!")
+print("Code sussesfully compiled!")
